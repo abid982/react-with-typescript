913c7cb6a2952edc3b8b2dd7f6a5243672ff3fde
abid982 <abidarif982@gmail.com>
2023-03-07 13:23:36 +0500

    exploring the base types

---
+++ basics.ts
@@ -0,0 +1,29 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives: number, string, boolean, null, symbols, undefined
+
+// Variable declaration
+// Type of the value
+// let age: number;
+
+// Assign a string gives an error
+// age = '12';
+
+// Set types
+let age: number;
+age = 12;
+
+let userName: string;
+
+// Lowercase starting character
+userName = 'Max';
+
+// Store boolean values
+let isInstructor: boolean;
+isInstructor = true;
+
+// null, undefined
+// let hobbies: null;
+// hobbies = 12; // error
